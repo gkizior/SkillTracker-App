19c852be97003d739eb575ae42a3bf779cc39e9a
Garrett Kizior <kiziorgarrett@gmail.com>
2018-06-05 13:17:59 -0500

    added getMatches

--- src/main/java/com/wmp/controller/SolrController.java
+++ src/main/java/com/wmp/controller/SolrController.java
@@ -2,16 +2,24 @@ package com.wmp.controller;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wmp.model.Skills;
 import com.wmp.model.Solr;
 import com.wmp.repository.SolrRepository;
 
@@ -71,16 +79,16 @@ public class SolrController {
 		}
 		return skills;
 	}
-	
+
 	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping("/getAllSkills")
 	public List<String> getAllSkills() {
 		List<String> skillNames = new ArrayList<String>();
 
 		for (Solr skill : this.solrRepository.findAll()) {
-			if(skill.getSkills() != null) {
-				for(String skillName : skill.getSkills()) {
-					if(!skillNames.contains(skillName)) {
+			if (skill.getSkills() != null) {
+				for (String skillName : skill.getSkills()) {
+					if (!skillNames.contains(skillName)) {
 						skillNames.add(skillName);
 					}
 				}
@@ -115,38 +123,37 @@ public class SolrController {
 
 		return emps;
 	}
-	
-	
+
 	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(value = "/getStats", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Stat> getStats() {
 		List<Solr> solrList = this.getAll();
 		List<Stat> stats = new ArrayList<Stat>();
-		for(Solr solr : solrList) {
-			if(solr.getSkills() != null) {
-				for(String skill : solr.getSkills()) {
+		for (Solr solr : solrList) {
+			if (solr.getSkills() != null) {
+				for (String skill : solr.getSkills()) {
 					boolean found = false;
-					for(Stat stat: stats) {
-						if(stat.getName().equals(skill)) {
+					for (Stat stat : stats) {
+						if (stat.getName().equals(skill)) {
 							found = true;
 							List<Series> addToSeries = stat.getSeries();
 							int index = stats.indexOf(stat);
 							boolean foundCareerLevel = false;
-							for(Series series : stat.getSeries()) {
-								if(series.getName().equals(solr.getCareerLevel())) {
+							for (Series series : stat.getSeries()) {
+								if (series.getName().equals(solr.getCareerLevel())) {
 									series.setValue(series.getValue() + 1);
 									foundCareerLevel = true;
 								}
 							}
-							if(!foundCareerLevel) {
+							if (!foundCareerLevel) {
 								addToSeries.add(new Series(solr.getCareerLevel(), 1));
 							}
 							stat.setSeries(addToSeries);
 							stats.set(index, stat);
 						}
 					}
-					if(!found) {
+					if (!found) {
 						Stat addStat = new Stat();
 						List<Series> addSeriesList = new ArrayList<Series>();
 						Series addSeries = new Series(solr.getCareerLevel(), 1);
@@ -161,13 +168,55 @@ public class SolrController {
 		return stats;
 	}
 
-	// @CrossOrigin(origins = "http://localhost:4200")
-	// @RequestMapping(value = "/postEmployee", method = RequestMethod.POST)
-	// public void postEmployee(@RequestBody Skill json) throws Exception {
-	// Random rand = new Random();
-	// json.setEmpId("" + rand.nextInt(10000));
-	// skillRepository.save(json);
-	// }
+	@CrossOrigin(origins = "http://localhost:4200")
+	@PostMapping("/getMatches")
+	public List<GraphMatch> getMatches(@Valid @RequestBody MatchBody matchBody) {
+		int teamSize = matchBody.getNumber();
+		List<String> skills = matchBody.getSkills();
+		if(matchBody.getNumber() == 0) {
+			teamSize = (int)this.solrRepository.count();
+			skills = this.getAllSkills();
+		}
+		List<Solr> emps = this.getAll();
+		List<Match> result = new ArrayList<Match>();
+
+		for (Solr emp : emps) {
+			if (emp.getSkills() != null) {
+				int number = 0;
+				for (String skill : skills) {
+					if (Arrays.asList(emp.getSkills()).contains(skill)) {
+						number++;
+					}
+				}
+				result.add(new Match(emp, (double) number));
+			}
+		}
+		if (result != null) {
+			Collections.sort(result);
+			Collections.reverse(result);
+		}
+		try {
+			if (teamSize == 0)
+				return getGraphMatch(result);
+			return getGraphMatch(result.subList(0, teamSize));
+		} catch (Exception e) {
+			return getGraphMatch(result);
+		}
+	}
+
+	public List<GraphMatch> getGraphMatch(List<Match> matches) {
+		List<GraphMatch> gMatch = new ArrayList<>();
+		for (Match match : matches) {
+			String percent = ("" + (int) (match.getCount()) + " skills");
+			GraphMatch gM = new GraphMatch(percent, 1);
+			if (gMatch.contains(gM)) {
+				gMatch.get(gMatch.indexOf(gM)).setValue(gMatch.get(gMatch.indexOf(gM)).getValue() + 1);
+			} else {
+				gMatch.add(gM);
+			}
+		}
+		return gMatch;
+	}
 
 	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping(value = "/findEmployee/{query}", method = RequestMethod.GET)
@@ -186,17 +235,127 @@ public class SolrController {
 		}
 		return emps;
 	}
-	
+
+	@SuppressWarnings("serial")
+	static class MatchBody implements Serializable {
+		int number;
+		List<String> skills;
+
+		public MatchBody(int number, List<String> skills) {
+			this.number = number;
+			this.skills = skills;
+		}
+
+		public MatchBody() {
+			this.number = 0;
+			this.skills = new ArrayList<>();
+		}
+
+		public int getNumber() {
+			return this.number;
+		}
+
+		public List<String> getSkills() {
+			return this.skills;
+		}
+
+		public void setNumber(int number) {
+			this.number = number;
+		}
+
+		public void setSkills(List<String> skills) {
+			this.skills = skills;
+		}
+	}
+
+	@SuppressWarnings("serial")
+	class GraphMatch implements Serializable {
+		String name;
+		int value;
+
+		public GraphMatch(String name, int value) {
+			this.name = name;
+			this.value = value;
+		}
+
+		public GraphMatch() {
+			this.name = null;
+			this.value = 0;
+		}
+
+		public String getName() {
+			return this.name;
+		}
+
+		public int getValue() {
+			return this.value;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public void setValue(int value) {
+			this.value = value;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (o instanceof GraphMatch) {
+				GraphMatch newO = (GraphMatch) o;
+				return this.name.equals(newO.getName());
+			}
+			return false;
+		}
+	}
+
+	@SuppressWarnings("serial")
+	class Match implements Serializable, Comparable<Match> {
+		Solr solr;
+		double count;
+
+		public Match(Solr solr, double count) {
+			this.solr = solr;
+			this.count = count;
+		}
+
+		public Match() {
+			this.solr = null;
+			this.count = 0;
+		}
+
+		public Solr getSolr() {
+			return this.solr;
+		}
+
+		public double getCount() {
+			return this.count;
+		}
+
+		public void setSolr(Solr solr) {
+			this.solr = solr;
+		}
+
+		public void setCount(double count) {
+			this.count = count;
+		}
+
+		@Override
+		public int compareTo(Match o) {
+			return (int) ((this.count * 100) - (o.count * 100));
+		}
+	}
+
 	@SuppressWarnings("serial")
 	class Stat implements Serializable {
 		String name;
 		List<Series> series;
-		
+
 		public Stat(String name, List<Series> series) {
 			this.name = name;
 			this.series = series;
 		}
-		
+
 		public Stat() {
 			this.name = null;
 			this.series = new ArrayList<Series>();
@@ -205,48 +364,48 @@ public class SolrController {
 		public String getName() {
 			return this.name;
 		}
-		
+
 		public List<Series> getSeries() {
 			return this.series;
 		}
-		
+
 		public void setName(String name) {
 			this.name = name;
 		}
-		
+
 		public void setSeries(List<Series> series) {
 			this.series = series;
 		}
 	}
-	
+
 	@SuppressWarnings("serial")
 	class Series implements Serializable {
-		
+
 		String name;
 		int value;
-		
+
 		public Series(String careerLevel, int i) {
 			this.name = careerLevel;
 			this.value = i;
 		}
-		
+
 		public Series() {
 			this.name = null;
 			this.value = 0;
 		}
-		
+
 		public String getName() {
 			return this.name;
 		}
-		
+
 		public int getValue() {
 			return this.value;
 		}
-		
+
 		public void setName(String name) {
 			this.name = name;
 		}
-		
+
 		public void setValue(int value) {
 			this.value = value;
 		}
